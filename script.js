fa15ee2c1a835b7731431cc1ddbac25452262e07
Steven-QQQ <3048175405@qq.com>
2023-05-30 17:00:01 +0800

    add program

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 5,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    // hide: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
